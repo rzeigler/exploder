32e8742d0eee90f8638ac09239afdc43f68618dc
Ryan Zeigler <ryan.zeigler@s5stratos.com>
2020-01-30 19:11:15 -0600

    Provide configuration of error behavior

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,30 +5,51 @@ import fs2._
 import scala.concurrent.duration._
 
 object Main extends IOApp {
-  val mayThrow: IO[Array[Int]] = IO.delay {
+  val mayThrowOOME: IO[Array[Int]] = IO.delay {
     val r = math.random()
     if (r < 0.1d) new Array[Int](Int.MaxValue)
     else new Array[Int]((r * 100).toInt)
+  } recoverWith {
+    case _: Throwable => IO(println("explode")).as(new Array[Int](0))
   }
 
-  val stream = Stream
-    .fixedRate(100.millis)
-    .evalMap(_ =>
-      mayThrow.recoverWith({
-        case _: Throwable => IO(println("explode")).as(new Array[Int](0))
-      })
-    )
+  val mayThrowRTE: IO[Array[Int]] = IO.delay {
+    val r = math.random()
+    if (r < 0.1d) throw new RuntimeException("boom")
+    else new Array[Int]((r * 100).toInt)
+  } recoverWith {
+    case _: NumberFormatException =>
+      IO(println("explode")).as(new Array[Int](0))
+  }
 
-  def run(args: List[String]): IO[ExitCode] =
+  val mayThrowRTECatch: IO[Array[Int]] = IO.delay {
+    val r = math.random()
+    if (r < 0.1d) throw new RuntimeException("boom")
+    else new Array[Int]((r * 100).toInt)
+  } recoverWith {
+    case _: Throwable =>
+      IO(println("explode")).as(new Array[Int](0))
+  }
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val stream =
+      if (args.head === "oome")
+        Stream.fixedRate(100.millis).evalMap(_ => mayThrowOOME)
+      else if (args.head === "rte")
+        Stream.fixedRate(100.millis).evalMap(_ => mayThrowRTE)
+      else if (args.head === "rte-catch")
+        Stream.fixedRate(100.millis).evalMap(_ => mayThrowRTECatch)
+      else
+        Stream.raiseError[IO](new RuntimeException("bad argument"))
     stream
       .holdResource(new Array[Int](0))
       .use(current =>
         Stream
-          .fixedRate(10.millis)
+          .fixedRate(50.millis)
           .evalMap(_ => current.get.flatMap(l => IO(println(l.length))))
           .compile
           .drain
       )
       .as(ExitCode.Success)
-
+  }
 }
